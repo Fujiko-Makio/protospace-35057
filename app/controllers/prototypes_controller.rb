24015fc18a43f3364b6ee96249153ba55839791b
Fujiko-Makio <fujiwaramasayuki@fujiwaramasayukinoMacBook-Air.local>
2021-04-06 13:51:20 +0900

    修正３

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,8 @@
 class PrototypesController < ApplicationController
-  before_action :authenticate_user!, only: [:new, :edit, :update, :destroy]
-  before_action :move_to_index, except: [:index, :show, :new]
-
+  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :set_prototype, except: [:index, :new, :create]
+  before_action :move_to_index, only: [:edit, :update, :destroy]
+  
   
   def index
 
@@ -49,12 +50,17 @@ class PrototypesController < ApplicationController
 
   private
   def prototype_params
+    
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
 
   def move_to_index
-    @prototype = Prototype.find(params[:id])
+    
     redirect_to root_path unless current_user == @prototype.user
+  end
+
+  def set_prototype
+    @prototype = Prototype.find(params[:id])
   end
 
  
